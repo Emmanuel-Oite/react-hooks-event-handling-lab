bb73a62c1cc5532ed5e08cc1528c1105d1bfc3f3
Emmanuel <opotiemmanuel@gmail.com>
2023-12-21 20:15:19 +0300

    Completes Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,28 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+// In the components/EyesOnMe.js file, create an EyesOnMe React component.
+// In that component, render a button with the text 'Eyes on me'.
+// On that button, add an event handler that listens for the focus and blur events.
+// When the focus event fires, use console.log to print out the text 'Good!'.
+// When the blur event fires, use console.log to print out the text 'Bad!'.
+
+function EyesOnMe() {
+    function handleFocus() {
+        console.log('Good!');
+    }
+
+    function handleBlur() {
+        console.log('Bad! Eyes on me!');
+    }
+
+    return (
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}>
+                Eyes on me
+            </button>
+        </div>
+    );
+}
+
+export default EyesOnMe;
